93f244de54745e5a1cbf68625cbd9f14f71b8485
gshahzer <gshahzer@gmail.com>
2024-04-20 13:33:45 +0500

    second commit

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,9 @@
 <?php
 
+use App\Http\Controllers\TestController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [TestController::class, 'index']);
 Route::get('/about', function () {
     return "about";
 });
